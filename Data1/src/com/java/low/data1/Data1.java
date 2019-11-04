69f0d2f8b2d8117b57645bd07290d25de9486498
wanasan <56948750+wanasan@users.noreply.github.com>
2019-11-05 07:15:24 +0900

    Add files via upload

---
+++ Data1/src/com/java/low/data1/Data1.java
@@ -0,0 +1,31 @@
+package com.java.low.data1;
+
+public class Data1 {
+	
+public static void main(String[] args)
+	{
+	
+		boolean t = true;
+		boolean f = false;
+		System.out.println("Boolean ture = " + t);
+        System.out.println("Boolean false = " + f);
+		
+		String myString = "Test my String";
+        System.out.println("MyString: " + myString);
+        System.out.println("String Length = " + myString.length() );
+        
+        int var1 = 10;
+        int var2 = 20;
+        System.out.println("Int var1: " + var1);
+        System.out.println("Int var2: " + var2);
+        System.out.println("var1+var2 = " + (var1 + var2));
+        
+        double db1 = 10.01234;
+    	double db2 = 20.56789;
+        System.out.println("Double db1: " + db1);
+        System.out.println("Double db2: " + db2);  
+        System.out.println("db1+db2 = " + (db1+db2));
+		
+	}
+
+}
